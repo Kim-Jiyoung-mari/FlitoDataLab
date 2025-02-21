7e6c8db2e45424d8029395c7d76c0b15861d536a
JI0 <alsn8539@gmail.com>
2025-02-21 17:10:47 +0900

    CSS완료

---
+++ script/index.js
@@ -0,0 +1,9 @@
+const mainS = new Swiper('#main',{
+    mousewheel:true,
+    direction:'vertical',
+    // on:{
+    //     slideChangeTransitionEnd:function(){
+    //         setTimeout(()=> ScrollTrigger.refresh() ,0)
+    //     }
+    // }
+})
